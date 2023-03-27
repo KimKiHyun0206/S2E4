c2e326a3e05061ca80c99e11bf925249d3197ac6
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-03-27 23:52:12 +0900

    fix : exception enum

--- src/main/java/com/projeckt/s2e4/exception/ErrorMessage.java
+++ src/main/java/com/projeckt/s2e4/exception/ErrorMessage.java
@@ -13,12 +13,12 @@ public enum ErrorMessage {
     CONFLICT_ERROR(HttpStatus.BAD_REQUEST, "예기치 못한 에러가 발생했습니다."),
     INTERNAL_SERVER_ERROR_BY_MAPPER_ERROR(HttpStatus.BAD_REQUEST, "예기치 못한 에러가 발생했습니다."),
     INTERNAL_SERVER_ERROR_BY_PROPERTIES_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 에러가 발생했습니다."),
-    INVALID_REQUEST_PARMAETER_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청 입니다."),
+    INVALID_REQUEST_PARAMETER_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청 입니다."),
 
     /**
-     * Todo Error Message
+     * Error Message
      */
-    TODO_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "Todo 정보를 찾을 수 없습니다."),
+    DATA_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "데이터 정보를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus status;
