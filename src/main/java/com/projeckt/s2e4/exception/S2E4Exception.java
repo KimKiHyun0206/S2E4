a11b63dc669f9f4f57d5fd8f2374eeb3637e7b11
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-03-27 23:46:32 +0900

    update : exception handler

---
+++ src/main/java/com/projeckt/s2e4/exception/S2E4Exception.java
@@ -0,0 +1,7 @@
+package com.projeckt.s2e4.exception;
+
+public class S2E4Exception extends BusinessException {
+    public S2E4Exception() {
+        super(ErrorMessage.TODO_NOT_FOUND_ERROR);
+    }
+}
