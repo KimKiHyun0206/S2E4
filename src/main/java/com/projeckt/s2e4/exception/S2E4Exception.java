807663fba739684e2dad2cc7d6ef1ba38e52f08e
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-03-27 23:52:38 +0900

    fix : change enum value

--- src/main/java/com/projeckt/s2e4/exception/S2E4Exception.java
+++ src/main/java/com/projeckt/s2e4/exception/S2E4Exception.java
@@ -2,6 +2,6 @@ package com.projeckt.s2e4.exception;
 
 public class S2E4Exception extends BusinessException {
     public S2E4Exception() {
-        super(ErrorMessage.TODO_NOT_FOUND_ERROR);
+        super(ErrorMessage.DATA_NOT_FOUND_ERROR);
     }
 }
