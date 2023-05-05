e5755b0555568b981f7fb18bf9b2d9f40d61e975
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-05-05 13:29:19 +0900

    update : duplicateValidation

--- src/main/java/com/projekt/s2e4/service/validation/user/UserRegisterValidation.java
+++ src/main/java/com/projekt/s2e4/service/validation/user/UserRegisterValidation.java
@@ -1,16 +1,21 @@
 package com.projekt.s2e4.service.validation.user;
 
 import com.projekt.s2e4.dto.user.request.UserRegisterRequest;
+import com.projekt.s2e4.entity.User;
 import com.projekt.s2e4.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserRegisterValidation {
     private final UserRepository userRepository;
+
     public boolean validation(UserRegisterRequest request) {
         return userNameValidation(request.getUserName());
     }
@@ -19,7 +24,31 @@ public class UserRegisterValidation {
         boolean isLengthUnderEight = userName.length() <= 8;
         boolean isOnlyEnglish = userName.equals("^[a-zA-Z]*$");
 
+
         return isLengthUnderEight & isOnlyEnglish;
+    }
+
+    private boolean isNotDuplicateUserName(String userName) {
+        List<String> userNames = getUsernamesFromRepository();
+
+        boolean returnValue = true;
+
+        for (int i = 0; i < userNames.size(); i++) {
+            if (userNames.get(i).equals(userName)) return false;
+        }
+        return true;
+    }
+
+    private List<String> getUsernamesFromRepository() {
+        List<User> users = userRepository.getAllUserName();
+
+        List<String> userNames = new LinkedList<>();
+
+        users.forEach(
+                user -> userNames.add(user.getUserName())
+        );
+
+        return userNames;
     }
 
 }
